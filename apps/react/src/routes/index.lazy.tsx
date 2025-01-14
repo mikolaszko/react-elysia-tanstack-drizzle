df38618686aa26c7d79a5934c7f54ad101128f28
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2025-01-14 22:54:09 +0100

    feat: add tanstack query

--- apps/react/src/routes/index.lazy.tsx
+++ apps/react/src/routes/index.lazy.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute('/')({
@@ -5,9 +6,29 @@ export const Route = createLazyFileRoute('/')({
 })
 
 function Index() {
+  const { isPending, error, data, isFetching } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: async () => {
+      const response = await fetch(
+        'https://api.github.com/repos/TanStack/query',
+      )
+      return await response.json()
+    },
+  })
+
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
+
   return (
     <div>
       <h3>Welcome Home!</h3>
+      <h1>{data.full_name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+      <div>{isFetching ? 'Updating...' : ''}</div>
     </div>
   )
 }
